0123b4442b876bd5f6f6be6cab56e420c24df39e
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-04-14 12:30:38 -0700

    added anchor links for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,16 @@ export default function Header(){
     return(
         <header>
             <h1 className= "title">Seattle</h1>
+
+            <nav>
+                <ul className= "nav-links">
+                    <li><a href= "#topic">Topic</a></li>
+                    <li><a href= "#Introduction">Introduction</a></li>
+                    <li><a href= "#funFacts">Fun Facts</a></li>
+                    <li><a href= "#tourist places">Tourist Places</a></li>
+                    <li><a href= "#footer">Footer</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
