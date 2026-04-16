10c7c83469798680860c5053808fe8952b167a75
Lilian <210286008+liliann19@users.noreply.github.com>
2026-04-16 10:48:44 -0700

    added styling for the heroSection and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,12 @@ export default function Header(){
     return(
         <header>
             <h1 className= "title">Seattle</h1>
-
             <nav>
                 <ul className= "nav-links">
                     <li><a href= "#topic">Topic</a></li>
-                    <li><a href= "#Introduction">Introduction</a></li>
+                    <li><a href= "#intro">Introduction</a></li>
                     <li><a href= "#funFacts">Fun Facts</a></li>
-                    <li><a href= "#tourist places">Tourist Places</a></li>
+                    <li><a href= "#tourist">Tourist Places</a></li>
                     <li><a href= "#footer">Footer</a></li>
                 </ul>
             </nav>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,17 @@
 import heroSeattle from "../assets/heroSeattle.jpeg"
-export default function HeroSection(){
-    return(
-        <section>
-            <h1 className= "Intro">The Emerald City</h1>
-            <p>Seattle, the "Emerald City," is a dynamic coastal seaport and the largest city in the 
-                Pacific Northwest. Situated on an isthmus between Puget Sound and Lake Washington 
-                in Washington state, it is renowned for its stunning natural surroundings, vibrant coffee culture, 
-                and significant tech industry, hosting giants like Amazon and Microsoft.</p>
-                <img src={heroSeattle} alt= "Seattle image"></img>
+import "./HeroSection.css"
+
+export default function HeroSection() {
+    return (
+        <section className="hero">
+            <div className="hero-text">
+                <h1 className="intro" id="intro">The Emerald City</h1>
+                <p>Seattle, the "Emerald City," is a dynamic coastal seaport and the largest city in the
+                    Pacific Northwest. Situated on an isthmus between Puget Sound and Lake Washington
+                    in Washington state, it is renowned for its stunning natural surroundings, vibrant coffee culture,
+                    and significant tech industry, hosting giants like Amazon and Microsoft.</p>
+            </div>
+            <img src={heroSeattle} alt="Seattle image"></img>
         </section>
     )
 }
